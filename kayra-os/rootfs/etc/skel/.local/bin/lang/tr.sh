b47d4d1b82e3d4e17bdc119d32f429bb13ba8ea7
Ali E. <43685426+alierkanimrek@users.noreply.github.com>
2026-09-25 13:23:33 +0300

    Create Turkish localization script tr.sh
    Add Turkish localization script for welcome and home usage notifications.

---
+++ kayra-os/rootfs/etc/skel/.local/bin/lang/tr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -A i18n=(
+  [WELCOME]="Hoşgeldiniz"
+)
+
+declare -A i18n_HOME_USAGE=(
+  [NOTIFY_TITLE]="Ev dizini boyutu"
+  [NOTIFY_MSG]="{HOME} Dizini {USAGE} yer kaplıyor."
+  [NOTIFY_BUTTON]="Baobab'ı Aç"
+)
